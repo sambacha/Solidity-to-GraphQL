4350643e4d1b95266d03ed1d87045362bda057ce
sam <sam@freighttrust.com>
2020-09-04 23:48:57 -0700

    fix: graphql generators
    Signed-off-by: sam <sam@freighttrust.com>

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require('express')
 const graphqlHTTP = require('express-graphql')
 
 const Web3 = require('web3')
-const provider = new Web3.providers.HttpProvider('http://localhost:9545')
+const provider = new Web3.providers.HttpProvider('http://localhost:8545')
 // const TFcontract = require('truffle-contract')
 
 const TFcontract = require('@truffle/contract')
